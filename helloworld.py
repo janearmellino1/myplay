d2fec9ec4bfc41faae5fa7e3431dbf728ad436c7
janearmellino1 <janearmellino@gmail.com>
2016-10-30 19:28:00 -0400

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,19 @@
-print 'Content-Type: text/plain'
-print ''
-print 'Hello there world, Happy Friday!'
+#print 'Content-Type: text/plain'
+#print ''
+#print 'Hello there world, Happy Friday!'
+
+# write-html-2-windows.py
+
+import webbrowser
+
+f = open('helloworld.html','w')
+
+message = """<html>
+<head></head>
+<body><p>Hello World - happy Sunday!</p></body>
+</html>"""
+
+f.write(message)
+f.close()
+
+webbrowser.open_new_tab('helloworld.html')
