7e0450cf6ad9b78a6996f113afeea14d6659c57c
janearmellino1 <janearmellino@gmail.com>
2016-10-30 20:53:45 -0400

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,25 @@
+import cgi
+import datetime
+import time
+import sys
+import urllib
+import webapp2
+import jinja2
+import os
+import webbrowser
+
+from array import *
+from datetime import *
+from time import *
+
+from google.appengine.api import users
+from google.appengine.api import urlfetch
+from google.appengine.api import oauth
+from google.appengine.ext import db
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
 #print 'Content-Type: text/plain'
 #print ''
 #print 'Hello there world, Happy Friday!'
@@ -5,17 +27,15 @@
 # write-html-2-windows.py
 
 # a simple template
-template = "<html><body><h1>Hello {who}!</h1></body></html>"
-print(template.format(who="Reader"))
+#template = "<html><body><h1>Hello {who}!</h1></body></html>"
+#print(template.format(who="Reader"))
 
-#import webbrowser
+f = open('helloworld.html','w')
 
-#f = open('helloworld.html','w')
-
-#message = """<html>
-#<head></head>
-#<body><p>Hello World - happy Sunday!</p></body>
-#</html>"""
+message = """<html>
+<head></head>
+<body><p>Hello World - happy Sunday!</p></body>
+</html>"""
 
 #f.write(message)
 #f.close()
