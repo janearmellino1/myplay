67f1145574500f9ec3b2efd46c7da4df509a2ec1
janearmellino1 <janearmellino@gmail.com>
2016-10-31 13:56:13 -0400

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -6,14 +6,16 @@ import urllib
 import webapp2
 import jinja2
 import os
+from array import *
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
+calendars = "https://calendar.google.com/calendar/embed?showTitle=0&amp;src=hcrhs.org_classroom4f06a38d%40group.calendar.google.com&ctz=America/New_York&amp;color=%230099FF
+&amp;src=hcrhs.org_classroom284e9a5f%40group.calendar.google.com&ctz=America/New_York&amp;showTitle=0&amp;color=%23A32929"
 
 template_values = {
-   				'cals' :  "https://calendar.google.com/calendar/embed?showTitle=0&amp;src=hcrhs.org_classroom4f06a38d%40group.calendar.google.com&ctz=America/New_York&amp;color=%230099FF
-&amp;src=hcrhs.org_classroom284e9a5f%40group.calendar.google.com&ctz=America/New_York&amp;showTitle=0&amp;color=%23A32929",
+   				'cals' :  calendars,
    				}
 
 # a simple template
