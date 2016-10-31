5ba68b3bfc3aebc949897a2612c3d7d9fe5804c9
janearmellino1 <janearmellino@gmail.com>
2016-10-31 13:12:49 -0400

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -10,9 +10,15 @@ import os
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
+# Template Variabes
+    template_values = {
+    'calendars' : "https://calendar.google.com/calendar/embed?showTitle=0&amp;src=hcrhs.org_classroom4f06a38d%40group.calendar.google.com&ctz=America/New_York&amp;color=%230099FF
+&amp;src=hcrhs.org_classroom284e9a5f%40group.calendar.google.com&ctz=America/New_York&amp;showTitle=0&amp;color=%23A32929" 
+    }
+
 # a simple template
 #template = "<html><body><h1>Hello {who}!</h1></body></html>"
 
-template = jinja_environment.get_template('play.html')
+template = jinja_environment.get_template('play.html(template_values)')
 #  				self.response.out.write(template.render(template_values))
 print(template.render())
