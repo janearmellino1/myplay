cc0b742b479dd1dffa46b1068f8952bed500d7dd
janearmellino1 <janearmellino@gmail.com>
2016-10-31 13:51:02 -0400

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -11,6 +11,11 @@ jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 
+template_values = {
+   				'cals' :  "https://calendar.google.com/calendar/embed?showTitle=0&amp;src=hcrhs.org_classroom4f06a38d%40group.calendar.google.com&ctz=America/New_York&amp;color=%230099FF
+&amp;src=hcrhs.org_classroom284e9a5f%40group.calendar.google.com&ctz=America/New_York&amp;showTitle=0&amp;color=%23A32929",
+   				}
+
 # a simple template
 #template = "<html><body><h1>Hello {who}!</h1></body></html>"
 
