f14a917da337900670796f379fc99f8607b3e837
janearmellino1 <janearmellino@gmail.com>
2016-10-30 22:25:28 -0400

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,15 +1,7 @@
-import cgi
-import datetime
-import time
-import sys
-import urllib
+
 import webapp2
 import webbrowser
 
-
-
-            # a simple template
-            template = "<html><body><h1>Hello {who}!</h1></body></html>"
-            print(template.format(who="Reader"))
-            
-  
+# a simple template
+template = "<html><body><h1>Hello {who}!</h1></body></html>"
+print(template.format(who="Reader"))
