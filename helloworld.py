cc8611f6ae4d38fdb6fe799f5c9ba97f71fb125e
janearmellino1 <janearmellino@gmail.com>
2016-10-31 12:49:24 -0400

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,13 @@
-
 import webapp2
 import webbrowser
+import jinja
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 # a simple template
-template = "<html><body><h1>Hello {who}!</h1></body></html>"
-print(template.format(who="Reader"))
+#template = "<html><body><h1>Hello {who}!</h1></body></html>"
+
+template = jinja_environment.get_template('index.html')
+#  				self.response.out.write(template.render(template_values))
+print(template.render())
