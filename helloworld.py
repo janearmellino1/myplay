96419e61aa07ff6b8b144034446922a2bd14b568
janearmellino1 <janearmellino@gmail.com>
2016-10-30 22:22:44 -0400

    Update and rename helloworld.app to helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+import cgi
+import datetime
+import time
+import sys
+import urllib
+import webapp2
+import webbrowser
+
+
+
+            # a simple template
+            template = "<html><body><h1>Hello {who}!</h1></body></html>"
+            print(template.format(who="Reader"))
+            
+  
