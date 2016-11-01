e29d5b580af004ed429a44d6070d5ae660141a28
janearmellino1 <janearmellino@gmail.com>
2016-11-01 19:45:07 -0400

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -16,20 +16,20 @@ jinja_environment = jinja2.Environment(
 #class MainHandler(webapp2.RequestHandler):
 #   def get(self):	
 #   try:
-           calendars = """https://calendar.google.com/calendar/embed?showTitle=0&amp;src=hcrhs.org_classroom4f06a38d%40group.calendar.google.com&ctz=America/New_York&amp;color=%230099FF
-            &amp;src=hcrhs.org_classroom284e9a5f%40group.calendar.google.com&ctz=America/New_York&amp;showTitle=0&amp;color=%23A32929"""
+calendars = """https://calendar.google.com/calendar/embed?showTitle=0&amp;src=hcrhs.org_classroom4f06a38d%40group.calendar.google.com&ctz=America/New_York&amp;color=%230099FF
+&amp;src=hcrhs.org_classroom284e9a5f%40group.calendar.google.com&ctz=America/New_York&amp;showTitle=0&amp;color=%23A32929"""
 
-            template_values = {
-   				'cals' :  calendars,
-                'myheader' : user,
-   				}
+template_values = {
+    'cals' :  calendars,
+    'myheader' : user,
+    }
 
-            # a simple template
-            #template = "<html><body><h1>Hello {who}!</h1></body></html>"
+# a simple template
+#template = "<html><body><h1>Hello {who}!</h1></body></html>"
 
-            template = jinja_environment.get_template('play.html')
-            #self.response.out.write(template.render(template_values))
-            print(template.render(template_values))
+template = jinja_environment.get_template('play.html')
+#self.response.out.write(template.render(template_values))
+print(template.render(template_values))
 
           
             #except oauth.OAuthRequestError, e:
