5813a4917c5b5b646c1af305d9d0edf895ec38ac
janearmellino1 <janearmellino@gmail.com>
2016-11-01 19:39:40 -0400

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -16,8 +16,6 @@ jinja_environment = jinja2.Environment(
 class MainHandler(webapp2.RequestHandler):
     def get(self):	
     	try:
-            user = users.get_current_user()
-
             calendars = """https://calendar.google.com/calendar/embed?showTitle=0&amp;src=hcrhs.org_classroom4f06a38d%40group.calendar.google.com&ctz=America/New_York&amp;color=%230099FF
             &amp;src=hcrhs.org_classroom284e9a5f%40group.calendar.google.com&ctz=America/New_York&amp;showTitle=0&amp;color=%23A32929"""
 
