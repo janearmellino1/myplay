9c25f3d68b6bfcf3f33199596f4dcd7a465553bc
janearmellino1 <janearmellino@gmail.com>
2016-11-01 19:43:04 -0400

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -13,10 +13,10 @@ jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):	
-    	try:
-            calendars = """https://calendar.google.com/calendar/embed?showTitle=0&amp;src=hcrhs.org_classroom4f06a38d%40group.calendar.google.com&ctz=America/New_York&amp;color=%230099FF
+#class MainHandler(webapp2.RequestHandler):
+#   def get(self):	
+#   try:
+           calendars = """https://calendar.google.com/calendar/embed?showTitle=0&amp;src=hcrhs.org_classroom4f06a38d%40group.calendar.google.com&ctz=America/New_York&amp;color=%230099FF
             &amp;src=hcrhs.org_classroom284e9a5f%40group.calendar.google.com&ctz=America/New_York&amp;showTitle=0&amp;color=%23A32929"""
 
             template_values = {
@@ -28,15 +28,15 @@ class MainHandler(webapp2.RequestHandler):
             #template = "<html><body><h1>Hello {who}!</h1></body></html>"
 
             template = jinja_environment.get_template('play.html')
-            self.response.out.write(template.render(template_values))
-            #print(template.render(template_values))
+            #self.response.out.write(template.render(template_values))
+            print(template.render(template_values))
 
           
-        except oauth.OAuthRequestError, e:
-        	self.redirect('/msg=You must allow this System Google Contact Access to use it')
+            #except oauth.OAuthRequestError, e:
+        	#self.redirect('/msg=You must allow this System Google Contact Access to use it')
 
 
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+#app = webapp2.WSGIApplication([
+#    ('/', MainHandler)
+#], debug=True)
