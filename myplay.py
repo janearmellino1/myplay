ddb40f9c4b36ab558818797654f520fc554d7094
janearmellino1 <janearmellino@gmail.com>
2016-11-01 22:41:30 -0400

    Update myplay.py

--- myplay.py
+++ myplay.py
@@ -13,18 +13,20 @@ jinja_environment = jinja2.Environment(
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        user = users.get_current_user()
-        if user:
-            nickname = user.nickname()
-            logout_url = users.create_logout_url('/')
-            greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
-                nickname, logout_url)
-        else:
-            login_url = users.create_login_url('/')
-            greeting = '<a href="{}">Sign in</a>'.format(login_url)
+        #user = users.get_current_user()
+        #if user:
+        #    nickname = user.nickname()
+        #    logout_url = users.create_logout_url('/')
+        #    greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
+        #        nickname, logout_url)
+        #else:
+        #    login_url = users.create_login_url('/')
+        #    greeting = '<a href="{}">Sign in</a>'.format(login_url)
 
-        self.response.write(
-            '<html><body>{}</body></html>'.format(greeting))
+        #self.response.write(
+        #    '<html><body>{}</body></html>'.format(greeting))
+        
+        self.response.write('<html><body><h1>Go to Bed Jane</h1></body></html>')
         
         # [START app]
         app = webapp2.WSGIApplication([
