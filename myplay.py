135431f7113bfd8f293070baf5a4c04822aad1f3
janearmellino1 <janearmellino@gmail.com>
2016-11-02 12:59:24 -0400

    Update myplay.py

--- myplay.py
+++ myplay.py
@@ -1,11 +1,11 @@
 import webapp2
 
-import jinja2
-import os
-from array import *
+#import jinja2
+#import os
+#from array import *
 
-jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+#jinja_environment = jinja2.Environment(
+#    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 
 class MainPage(webapp2.RequestHandler):
