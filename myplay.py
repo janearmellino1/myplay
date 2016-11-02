6f294210f9ec734b47f85e1df3e9483e10aaad43
janearmellino1 <janearmellino@gmail.com>
2016-11-02 12:44:15 -0400

    Update myplay.py

--- myplay.py
+++ myplay.py
@@ -1,5 +1,12 @@
 import webapp2
 
+import jinja2
+import os
+from array import *
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
@@ -29,11 +36,25 @@ class MainPage(webapp2.RequestHandler):
 class Calendars(webapp2.RequestHandler):
     def post(self):
         username = self.request.get("std_id")
-        welcome_string = """<html><body>
-                          Hi there, {}!
-                          </body></html>""".format(username)
-        self.response.headers["Content-Type"] = "text/html"
-        self.response.write(welcome_string)
+#        welcome_string = """<html><body>
+#                          Hi there, {}!
+#                         </body></html>""".format(username)
+#       self.response.headers["Content-Type"] = "text/html"
+#       self.response.write(welcome_string)
+        calendars = """https://calendar.google.com/calendar/embed?showTitle=0&amp;src=hcrhs.org_classroom4f06a38d%40group.calendar.google.com&ctz=America/New_York&amp;color=%230099FF
+            &amp;src=hcrhs.org_classroom284e9a5f%40group.calendar.google.com&ctz=America/New_York&amp;showTitle=0&amp;color=%23A32929"""
+
+        template_values = {
+   				    'cals' :  calendars,
+                'myheader' : user,
+   				}
+
+# a simple template
+#template = "<html><body><h1>Hello {who}!</h1></body></html>"
+
+        template = jinja_environment.get_template('play.html')
+#  				self.response.out.write(template.render(template_values))
+        print(template.render(template_values))
 
 
 routes = [('/', MainPage), ('/assigninfo', Calendars)]
