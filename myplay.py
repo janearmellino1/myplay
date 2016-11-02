f45cee7c1b41dea1b73190122be264717d2bb707
janearmellino1 <janearmellino@gmail.com>
2016-11-02 14:18:55 -0400

    Update myplay.py

--- myplay.py
+++ myplay.py
@@ -1,11 +1,11 @@
 import webapp2
-import jinja2
-import os
+#import jinja2
+#import os
 
-from google.appengine.api import users
-from google.appengine.api import urlfetch
-from google.appengine.api import oauth
-from google.appengine.ext import db
+#from google.appengine.api import users
+#from google.appengine.api import urlfetch
+#from google.appengine.api import oauth
+#from google.appengine.ext import db
 
 # jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
