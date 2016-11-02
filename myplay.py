fe631c6b0e5d4694be776727b78aa25458d58a56
janearmellino1 <janearmellino@gmail.com>
2016-11-01 22:26:19 -0400

    Update myplay.py

--- myplay.py
+++ myplay.py
@@ -12,19 +12,17 @@ from google.appengine.api import users
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
-user = users.get_current_user()
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            nickname = user.nickname()
+            logout_url = users.create_logout_url('/')
+            greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
+                nickname, logout_url)
+        else:
+            login_url = users.create_login_url('/')
+            greeting = '<a href="{}">Sign in</a>'.format(login_url)
 
-calendars = """https://calendar.google.com/calendar/embed?showTitle=0&amp;src=hcrhs.org_classroom4f06a38d%40group.calendar.google.com&ctz=America/New_York&amp;color=%230099FF
-&amp;src=hcrhs.org_classroom284e9a5f%40group.calendar.google.com&ctz=America/New_York&amp;showTitle=0&amp;color=%23A32929"""
-
-template_values = {
-   				'cals' :  calendars,
-            'myheader' : user,
-   				}
-
-# a simple template
-#template = "<html><body><h1>Hello {who}!</h1></body></html>"
-
-template = jinja_environment.get_template('play.html')
-#  				self.response.out.write(template.render(template_values))
-print(template.render(template_values))
+        self.response.write(
+            '<html><body>{}</body></html>'.format(greeting))
