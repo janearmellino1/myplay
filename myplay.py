573a93d4d586c6ff792746b4d7164c3f2b4d86ab
janearmellino1 <janearmellino@gmail.com>
2016-11-02 23:56:16 -0400

    Update myplay.py

--- myplay.py
+++ myplay.py
@@ -35,15 +35,11 @@ class MainPage(webapp2.RequestHandler):
             </body>
             </html>""")
 
+        
 
 class Calendars(webapp2.RequestHandler):
     def post(self):
         user = self.request.get("std_id")
-#        welcome_string = """<html><body>
-#                          Hi there, {}!
-#                         </body></html>""".format(user)
-#        self.response.headers["Content-Type"] = "text/html"
-#        self.response.write(welcome_string)
         calendars = """https://calendar.google.com/calendar/embed?showTitle=0&amp;src=hcrhs.org_classroom4f06a38d%40group.calendar.google.com&ctz=America/New_York&amp;color=%230099FF
             &amp;src=hcrhs.org_classroom284e9a5f%40group.calendar.google.com&ctz=America/New_York&amp;showTitle=0&amp;color=%23A32929"""
         template_values = {
@@ -53,8 +49,8 @@ class Calendars(webapp2.RequestHandler):
 
         template = jinja_environment.get_template('play.html')
         self.response.write(template.render(template_values))
-#       print(template.render(template_values))
 
+        
 
 routes = [('/', MainPage), ('/assigninfo', Calendars)]
 my_app = webapp2.WSGIApplication(routes, debug=True)
