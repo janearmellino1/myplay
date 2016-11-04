29301d45791294e8226523f48d816fda2fd66db7
janearmellino1 <janearmellino@gmail.com>
2016-11-04 14:07:11 -0400

    Update myplay.py

--- myplay.py
+++ myplay.py
@@ -11,19 +11,23 @@ import os
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
+def formatCals(id):
+    cals="""https://calendar.google.com/calendar/embed?showTitle=0&amp;src=hcrhs.org_classroom4f06a38d%40group.calendar.google.com&ctz=America/New_York&amp;color=%230099FF
+            &amp;src=hcrhs.org_classroom284e9a5f%40group.calendar.google.com&ctz=America/New_York&amp;showTitle=0&amp;color=%23A32929"""
+    return(cals)
+
+
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
          template = jinja_environment.get_template('play2.html')
          self.response.write(template.render())
-
-
         
 
 class Calendars(webapp2.RequestHandler):
     def post(self):
         user = self.request.get("std_id")
-        calendars = """https://calendar.google.com/calendar/embed?showTitle=0&amp;src=hcrhs.org_classroom4f06a38d%40group.calendar.google.com&ctz=America/New_York&amp;color=%230099FF
-            &amp;src=hcrhs.org_classroom284e9a5f%40group.calendar.google.com&ctz=America/New_York&amp;showTitle=0&amp;color=%23A32929"""
+        calendars = formatCals(user)
         template_values = {
                 'cals' :  calendars,
                 'myheader' : user,
@@ -32,7 +36,9 @@ class Calendars(webapp2.RequestHandler):
         template = jinja_environment.get_template('play.html')
         self.response.write(template.render(template_values))
 
-        
+  
+
+
 
 routes = [('/', MainPage), ('/assigninfo', Calendars)]
 my_app = webapp2.WSGIApplication(routes, debug=True)
