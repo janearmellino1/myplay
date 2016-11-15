8a857d3ce05d32da3c158f078d56c13179687797
janearmellino1 <janearmellino@gmail.com>
2016-11-15 09:40:06 -0500

    Update myplay.py

--- myplay.py
+++ myplay.py
@@ -43,9 +43,7 @@ class Calendars(webapp2.RequestHandler):
     def post(self):
         user = self.request.get("std_id")
         #calendars = formatCals(user)
-	calendars = """https://calendar.google.com/calendar/embed?showTitle=0&amp;src=hcrhs.org_classroom4f06a38d%40
-	    group.calendar.google.com&ctz=America/New_York&amp;color=%230099FF&amp;src=hcrhs.org_classroom284e9a5f%40
-	    group.calendar.google.com&ctz=America/New_York&amp;showTitle=0&amp;color=%23A32929"""
+	calendars = """https://calendar.google.com/calendar/embed?showTitle=0&amp;src=hcrhs.org_classroom4f06a38d%40group.calendar.google.com&ctz=America/New_York&amp;color=%230099FF&amp;src=hcrhs.org_classroom284e9a5f%40group.calendar.google.com&ctz=America/New_York&amp;showTitle=0&amp;color=%23A32929"""
 	#timeraw = datetime.datetime.now(tz=pytz.est)
         template_values = {
                 'cals' :  calendars,
