cc835339cc6456666469471be742c59877e4f03e
janearmellino1 <janearmellino@gmail.com>
2016-11-15 09:17:44 -0500

    Update myplay.py

--- myplay.py
+++ myplay.py
@@ -43,7 +43,7 @@ class Calendars(webapp2.RequestHandler):
     def post(self):
         user = self.request.get("std_id")
         calendars = formatCals(user)
-	//timeraw = datetime.datetime.now(tz=pytz.est)
+	#timeraw = datetime.datetime.now(tz=pytz.est)
         template_values = {
                 'cals' :  calendars,
                 'myheader' : user,
